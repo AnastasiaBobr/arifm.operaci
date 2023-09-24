54452c5ebe61e7a69e766ee88a68a2f20b1c1476
Anastasia Bobrovskaya <Anastasia Bobrovskaya>
2023-09-24 18:10:09 +0700

    first commit

---
+++ variant1.py
@@ -0,0 +1,8 @@
+import math
+a = float(input('Введите a: '))
+
+z1=2*math.sin(3*math.pi-2*a)**2*math.cos(5*math.pi+2*a)**2
+z2=1/4-1/4*math.sin(5/2*math.pi-8*a)
+
+print(f'z1={z1}')
+print(f'z2={z2}')
